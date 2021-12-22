f9575a7977fa553a3b4aa642bb155da98a48d68f
Nataniel Santos <nathan.santos159@hotmail.com>
2021-12-22 17:20:36 -0300

    Criando data.js

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const projects = [
+  {
+    name: 'TrybeTunes',
+    description: 'Uma aplicação web para pesquisar e salvar as músicas favoritas',
+    image: 'https://github.com/natanielsantos159/trybetunes/raw/main/src/images/trybetuneshomepage.png',
+    repository: 'https://github.com/natanielsantos159/trybetunes',
+    link: 'https://natanielsantos159.github.io/trybetunes/'
+  },
+  {
+    name: 'TrybeWallet',
+    description: 'TrybeWallet é uma aplicação feita pra gerenciar despesas, com conversão de moedas de diversos países.',
+    image: 'https://github.com/natanielsantos159/trybetunes/raw/main/src/images/trybetuneshomepage.png',
+    repository: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
+    link: 'https://natanielsantos159.github.io/trybe-wallet'
+  },
+  {
+    name: 'Todo List',
+    description: 'Uma aplicação simples de lista de tarefas',
+    image: 'https://github.com/natanielsantos159/todo-list-project/raw/main/Captura%20de%20tela%20de%202021-12-22%2017-06-03.png',
+    repository: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
+    link: 'https://natanielsantos159.github.io/trybe-wallet'
+  },
+];
+
+export default projects;
