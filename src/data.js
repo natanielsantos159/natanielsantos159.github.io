0a890cf361d705a75ab2cd3bdb88b349a9efbbc4
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-08 14:08:28 -0300

    Adiciona projeto Trivia

--- src/data.js
+++ src/data.js
@@ -1,5 +1,19 @@
 const projects = [
   {
+    name: 'Trivia',
+    description: 'Um game de perguntas que utiliza a API do Trivia',
+    image: 'https://github.com/natanielsantos159/trivia-redux/raw/main-group-16/public/trivia.gif',
+    repository: 'https://github.com/natanielsantos159/trivia-redux',
+    link: 'https://natanielsantos159.github.io/trivia-redux/'
+  },
+  {
+    name: 'TrybeWallet',
+    description: 'TrybeWallet é uma aplicação feita pra gerenciar despesas, com conversão de moedas de diversos países.',
+    image: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
+    repository: 'https://github.com/natanielsantos159/trybe-wallet',
+    link: 'https://natanielsantos159.github.io/trybe-wallet'
+  },
+  {
     name: 'TrybeTunes',
     description: 'Uma aplicação web para pesquisar e salvar as músicas favoritas',
     image: 'https://github.com/natanielsantos159/trybetunes/raw/main/src/images/trybetuneshomepage.png',
@@ -7,18 +21,11 @@ const projects = [
     link: 'https://natanielsantos159.github.io/trybetunes/'
   },
   {
-    name: 'TrybeWallet',
-    description: 'TrybeWallet é uma aplicação feita pra gerenciar despesas, com conversão de moedas de diversos países.',
-    image: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
-    repository: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
-    link: 'https://natanielsantos159.github.io/trybe-wallet'
-  },
-  {
     name: 'Todo List',
     description: 'Uma aplicação simples de lista de tarefas',
     image: 'https://github.com/natanielsantos159/todo-list-project/raw/main/Captura%20de%20tela%20de%202021-12-22%2017-06-03.png',
-    repository: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
-    link: 'https://natanielsantos159.github.io/trybe-wallet'
+    repository: 'https://github.com/natanielsantos159/todo-list-project',
+    link: 'https://natanielsantos159.github.io/todo-list-project'
   },
 ];
 
