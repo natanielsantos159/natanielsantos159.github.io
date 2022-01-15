de75d65755fea690939d2c03cbef0d35a77db6e1
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-15 15:29:22 -0300

    Cria container AboutMe na pagina inicial

---
+++ src/containers/AboutMe.js
@@ -0,0 +1,24 @@
+import React from "react";
+import profilePicture from "../images/profile-picture.jpg";
+
+export default function AboutMe() {
+  return (
+    <div className="welcome-container">
+      <div className="info-wrapper">
+        <div>
+          <h1>Oii, tudo bem?</h1>
+          <p className="about-me-home">
+            Me chamo Nataniel Santos, tenho 20 anos e moro em Santa inês - MA.
+            Atualmente estou estudando Desenvolvimento Web(Full Stack) na Trybe.
+            Sempre fui apaixonado por tecnologia, já tenho um bom conhecimento em front-end e estou dando os meus primeiros passos em back-end!
+          </p>
+        </div>
+        <img
+          className="profile-picture"
+          src={profilePicture}
+          alt="Foto de Perfil"
+        />
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
-import React from 'react'
-
+import React from "react";
+import AboutMe from '../containers/AboutMe';
+import "../styles/Home.css";
 export default function Home() {
-  return (
-    <div>
-    </div>
-  )
+  return <main className="homepage">
+    <AboutMe />
+  </main>;
 }
