0980682d46f20e3997ef5f3c1d410ef7fb7bfb24
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-17 17:47:27 -0300

    Adiciona skills no arquivo data.js

--- src/data.js
+++ src/data.js
@@ -1,52 +1,89 @@
 const projects = [
   {
-    name: 'Trivia',
-    description: 'Um game de perguntas que utiliza a API do Trivia',
-    image: 'https://github.com/natanielsantos159/trivia-redux/raw/main-group-16/public/trivia.gif',
-    repository: 'https://github.com/natanielsantos159/trivia-redux',
-    website: 'https://natanielsantos159.github.io/trivia-redux/',
-    tags: ['Módulo de Front-End', 'Projeto em Grupo']
+    name: "Trivia",
+    description: "Um game de perguntas que utiliza a API do Trivia",
+    image:
+      "https://github.com/natanielsantos159/trivia-redux/raw/main-group-16/public/trivia.gif",
+    repository: "https://github.com/natanielsantos159/trivia-redux",
+    website: "https://natanielsantos159.github.io/trivia-redux/",
+    tags: ["Módulo de Front-End", "Projeto em Grupo"],
+    skills: [
+      'Criar um store Redux em aplicações React',
+      'Criar reducers no Redux em aplicações React',
+      'Criar actions no Redux em aplicações React',
+      'Criar dispatchers no Redux em aplicações React',
+      'Conectar Redux aos componentes React',
+      'Criar actions assíncronas na sua aplicação React que faz uso de Redux.',
+    ],
   },
   {
-    name: 'TrybeWallet',
-    description: 'TrybeWallet é uma aplicação feita pra gerenciar despesas, com conversão de moedas de diversos países.',
-    image: 'https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif',
-    repository: 'https://github.com/natanielsantos159/trybe-wallet',
-    website: 'https://natanielsantos159.github.io/trybe-wallet',
-    tags: ['Módulo de Front-End']
+    name: "TrybeWallet",
+    description:
+      "TrybeWallet é uma aplicação feita pra gerenciar despesas, com conversão de moedas de diversos países.",
+    image:
+      "https://github.com/natanielsantos159/trybe-wallet/raw/main/public/ezgif.com-gif-maker%20(2).gif",
+    repository: "https://github.com/natanielsantos159/trybe-wallet",
+    website: "https://natanielsantos159.github.io/trybe-wallet",
+    tags: ["Módulo de Front-End"],
+    skills: [
+      "Criar um store Redux em aplicações React",
+      "Criar reducers no Redux em aplicações React",
+      "Criar actions no Redux em aplicações React",
+      "Criar dispatchers no Redux em aplicações React",
+      "Conectar Redux aos componentes React",
+      "Criar actions assíncronas na sua aplicação React que faz uso de Redux.",
+    ],
   },
   {
-    name: 'TrybeTunes',
-    description: 'Uma aplicação web para pesquisar e salvar as músicas favoritas',
-    image: 'https://github.com/natanielsantos159/trybetunes/raw/main/src/images/trybetuneshomepage.png',
-    repository: 'https://github.com/natanielsantos159/trybetunes',
-    website: 'https://natanielsantos159.github.io/trybetunes/',
-    tags: ['Módulo de Front-End']
+    name: "TrybeTunes",
+    description:
+      "Uma aplicação web para pesquisar e salvar as músicas favoritas",
+    image:
+      "https://github.com/natanielsantos159/trybetunes/raw/main/src/images/trybetuneshomepage.png",
+    repository: "https://github.com/natanielsantos159/trybetunes",
+    website: "https://natanielsantos159.github.io/trybetunes/",
+    tags: ["Módulo de Front-End"],
+    skills: [],
   },
   {
-    name: 'Todo List',
-    description: 'Uma aplicação simples de lista de tarefas',
-    image: 'https://github.com/natanielsantos159/todo-list-project/raw/main/Captura%20de%20tela%20de%202021-12-22%2017-06-03.png',
-    repository: 'https://github.com/natanielsantos159/todo-list-project',
-    website: 'https://natanielsantos159.github.io/todo-list-project',
-    tags: ['Módulo de Fundamentos']
+    name: "Todo List",
+    description: "Uma aplicação simples de lista de tarefas",
+    image:
+      "https://github.com/natanielsantos159/todo-list-project/raw/main/Captura%20de%20tela%20de%202021-12-22%2017-06-03.png",
+    repository: "https://github.com/natanielsantos159/todo-list-project",
+    website: "https://natanielsantos159.github.io/todo-list-project",
+    tags: ["Módulo de Fundamentos"],
+    skills: ["Manipular CSS", "Manipular Javascript"],
   },
   {
-    name: 'Pixels Art',
-    description: 'App simples pra desenhar pixel art',
-    image: 'https://github.com/natanielsantos159/pixels-art-project/blob/nataniel-santos-project-pixels-art/Captura%20de%20tela%20de%202022-01-17%2014-43-46.png?raw=true',
-    repository: 'https://github.com/natanielsantos159/pixels-art-project',
-    website: 'https://natanielsantos159.github.io/pixels-art-project/',
-    tags: ['Módulo de Fundamentos']
+    name: "Pixels Art",
+    description: "App simples pra desenhar pixel art",
+    image:
+      "https://github.com/natanielsantos159/pixels-art-project/blob/nataniel-santos-project-pixels-art/Captura%20de%20tela%20de%202022-01-17%2014-43-46.png?raw=true",
+    repository: "https://github.com/natanielsantos159/pixels-art-project",
+    website: "https://natanielsantos159.github.io/pixels-art-project/",
+    tags: ["Módulo de Fundamentos"],
+    skills: ["Manipular o DOM", "Manipular o Javascript"],
   },
   {
-    name: 'Recipes App',
-    description: 'Um app de receitas de bebibas e comidas completo.',
-    image: 'https://github.com/natanielsantos159/recipes-app-project/blob/main-group-18-development/src/images/Captura%20de%20tela%20de%202022-01-17%2016-37-13.png?raw=true',
-    repository: 'https://github.com/natanielsantos159/recipes-app-project',
-    website: 'https://natanielsantos159.github.io/recipes-app-project/',
-    tags: ['Módulo de Front-End', 'Projeto em Grupo']
-  }
+    name: "Recipes App",
+    description: "Um app de receitas de bebibas e comidas completo.",
+    image:
+      "https://github.com/natanielsantos159/recipes-app-project/blob/main-group-18-development/src/images/Captura%20de%20tela%20de%202022-01-17%2016-37-13.png?raw=true",
+    repository: "https://github.com/natanielsantos159/recipes-app-project",
+    website: "https://natanielsantos159.github.io/recipes-app-project/",
+    tags: ["Módulo de Front-End", "Projeto em Grupo"],
+    skills: [
+      "Utilizar Redux para gerenciar estado",
+      "Utilizar a biblioteca React-Redux",
+      "Utilizar a Context API do React para gerenciar estado",
+      "Utilizar o React Hook useState",
+      "Utilizar o React Hook useContext",
+      "Utilizar o React Hook useEffect",
+      "Criar Hooks customizados",
+    ],
+  },
 ];
+
 
 export default projects;
