bbb356545d1fdc895ad16bca37e7d32deb911549
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-17 19:36:31 -0300

    Suporte a markdown na lista de skill

--- src/pages/ProjectSkills.js
+++ src/pages/ProjectSkills.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/ProjectSkills.css';
+import ReactMarkdown from 'react-markdown';
 
 export default function ProjectSkills({name, skills}) {
   console.log(skills)
@@ -7,7 +8,7 @@ export default function ProjectSkills({name, skills}) {
     <section className="project-skills">
       <h1>{name}</h1> 
       <ul className="skills-list">
-      {skills && skills.map((skill, i)=> <li key={i} className="skill">{skill}</li> )}
+      {skills && skills.map((skill, i)=> <li key={i} className="skill"><ReactMarkdown>{skill}</ReactMarkdown></li> )}
       </ul>
     </section>
   )
