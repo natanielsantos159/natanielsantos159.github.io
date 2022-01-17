7bd84cc0b614fe05a2563322fe553c6eacfec3d1
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-17 20:53:30 -0300

    Implementa botão de fechar modal de skills

--- src/pages/ProjectSkills.js
+++ src/pages/ProjectSkills.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../styles/ProjectSkills.css";
 import ReactMarkdown from "react-markdown";
+import closeIcon from "../images/close.png";
+import AppContext from "../context/AppContext";
 
 export default function ProjectSkills({ name, skills }) {
+  const { setShowSkills } = useContext(AppContext);
   return (
     <>
       <div id="mask"></div>
       <section className="project-skills">
+        <button
+          className="close-btn"
+          onClick={() => {
+            setShowSkills(false);
+            document.body.style.overflow = "scroll";
+          }}
+        >
+          <img src={closeIcon} alt="Fechar" />
+        </button>
         <h1>{name}</h1>
         <ul className="skills-list">
           {skills &&
