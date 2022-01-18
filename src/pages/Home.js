e2df53e250502b8d74378f639135715a58b453eb
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-17 21:30:05 -0300

    Implement modal de skills na tela inicial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import AboutMe from "../containers/AboutMe";
 import ContactMe from "../containers/ContactMe";
 import ProjectsContainer from "../containers/ProjectsContainer";
 import "../styles/Home.css";
 import Parallax from "react-rellax";
+import ProjectSkills from "./ProjectSkills";
+import projects from "../data";
+import AppContext from "../context/AppContext";
 
 export default function Home() {
+  const { showSkills } = useContext(AppContext);
+  const { idSkill } = useContext(AppContext);
+
   return (
     <main className="homepage">
       <AboutMe />
@@ -13,6 +19,12 @@ export default function Home() {
         <ProjectsContainer />
         <ContactMe />
       </Parallax>
+      {showSkills && idSkill && (
+        <ProjectSkills
+          name={projects[idSkill - 1].name}
+          skills={projects[idSkill - 1].skills}
+        />
+      )}
     </main>
   );
 }
