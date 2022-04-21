c18a0ca3daf6de372da98f8ac32ecab424d36d92
Nataniel Santos <nathan.santos159@hotmail.com>
2022-04-20 23:43:51 -0300

    Desinstala react-rellax

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import AboutMe from "../containers/AboutMe";
 import ContactMe from "../containers/ContactMe";
 import ProjectsContainer from "../containers/ProjectsContainer";
 import "../styles/Home.css";
-import Parallax from "react-rellax";
 import ProjectSkills from "./ProjectSkills";
 import { Element } from "react-scroll";
 
@@ -11,12 +10,10 @@ export default function Home() {
   return (
     <main className="homepage">
       <AboutMe />
-      <Parallax speed={0.6}>
         <ProjectsContainer />
         <Element name="contact-container">
           <ContactMe />
         </Element>
-      </Parallax>
       <ProjectSkills />
     </main>
   );
