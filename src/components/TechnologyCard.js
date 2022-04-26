24348f8d33f00822c8c43c0aa8a321759d0a4f0a
Nataniel Santos <nathan.santos159@hotmail.com>
2022-04-25 23:26:07 -0300

    Adiciona novas tecnologias e altera icones

--- src/components/TechnologyCard.js
+++ src/components/TechnologyCard.js
@@ -1,17 +1,23 @@
-import PropTypes from "prop-types"
-import React from 'react'
-import '../styles/TechnologyCard.css';
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import "../styles/TechnologyCard.css";
 
-export default function TechnologyCard({iconClass, name}) {
+export default function TechnologyCard({ iconClass, name }) {
+  const [hover, setHover] = useState(false);
+
   return (
-    <div className="technology-card">
-      <i className={iconClass} ></i>
+    <div
+      className="technology-card"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <i className={`${iconClass} ${hover ? 'colored' : ''}`}></i>
       <h3>{name}</h3>
     </div>
-  )
+  );
 }
 
 TechnologyCard.propTypes = {
   iconClass: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-}
+};
--- src/containers/Technologies.js
+++ src/containers/Technologies.js
@@ -10,14 +10,18 @@ export default function Technologies() {
     <section className="technologies-container" ref={ref}>
       <h1>Conhecimentos</h1>
       <section className="technologies-wrapper">
-        <TechnologyCard name="Node JS" iconClass="bx bxl-nodejs"/>
-        <TechnologyCard name="React JS" iconClass="bx bxl-react"/>
-        <TechnologyCard name="Redux" iconClass="bx bxl-redux"/>
-        <TechnologyCard name="CSS3" iconClass="bx bxl-css3"/>
-        <TechnologyCard name="HTML5" iconClass="bx bxl-html5"/>
-        <TechnologyCard name="Javascript" iconClass="bx bxl-javascript"/>
-        <TechnologyCard name="Docker" iconClass="bx bxl-docker"/>
-        <TechnologyCard name="Git" iconClass="bx bxl-git"/>
+        <TechnologyCard name="Node JS" iconClass="devicon-nodejs-plain"/>
+        <TechnologyCard name="React JS" iconClass="devicon-react-plain"/>
+        <TechnologyCard name="TypeScript" iconClass="devicon-typescript-plain"/>      
+        <TechnologyCard name="Redux" iconClass="devicon-redux-plain"/>
+        <TechnologyCard name="CSS3" iconClass="devicon-css3-plain"/>
+        <TechnologyCard name="HTML5" iconClass="devicon-html5-plain"/>
+        <TechnologyCard name="Javascript" iconClass="devicon-javascript-plain"/>
+        <TechnologyCard name="Express" iconClass="devicon-express-original"/>
+        <TechnologyCard name="Docker" iconClass="devicon-docker-plain"/>
+        <TechnologyCard name="Sequelize" iconClass="devicon-sequelize-plain"/>          
+        <TechnologyCard name="Jest" iconClass="devicon-jest-plain"/>          
+        <TechnologyCard name="Git" iconClass="devicon-git-plain"/>  
       </section>
     </section>
   )
