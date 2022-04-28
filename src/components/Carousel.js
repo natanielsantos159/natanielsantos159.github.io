3c3d0abebe64e0664a6ca905f47123f8e9e9ab48
Nataniel Santos <nathan.santos159@hotmail.com>
2022-04-28 15:02:31 -0300

    Corrige animação do carrosel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -5,6 +5,7 @@ import projects from '../projects';
 import { IconButton } from '@mui/material';
 import arrowLeftIcon from '../images/arrow-left.png';
 import arrowRightIcon from '../images/arrow-right.png';
+import { motion } from 'framer-motion';
 
 export default function Carousel() {
   const [carouselProjects] = useState(projects.slice(0, 6));
@@ -36,24 +37,22 @@ export default function Carousel() {
   const framerMotionProps = {
     drag: 'x',
     transformTemplate: template,
-    dragConstraints: { left: 10, right: 10 },
-    onDragEnd: (_event, info) => { 
-      if(info.offset.x > 150) nextItem();
-      if(info.offset.x < -150) previousItem();
+    dragConstraints: { left: 15, right: 15 },
+    onDragEnd: (_event, info) => {
+      if (info.offset.x > 150 || info.velocity.x > 100) nextItem();
+      if (info.offset.x < -150 || info.velocity.x < -100) previousItem();
     },
-    dragSnapToOrigin: false,
-    dragTransition:{ bounceStiffness: 300 },
-    layout: true,
+    dragSnapToOrigin: false
   };
 
   return (
-    <section className="carrousel-mode">
+    <motion.section className="carrousel-mode">
       {carouselProjects.map((proj, i) => (
         <ProjectCard
           {...proj}
-          key={i}
-          carouselClass={carouselClasses[i]}
-          framerMotionProps={framerMotionProps}
+            carouselClass={carouselClasses[i]}
+            key={i}
+          framerMotionProps={{...framerMotionProps }}
         />
       ))}
       <IconButton height="small" sx={{ position: 'absolute', top: '50%', left: '20%', zIndex: 6 }}>
@@ -62,6 +61,6 @@ export default function Carousel() {
       <IconButton size="small" sx={{ position: 'absolute', top: '50%', left: '80%', zIndex: 6 }}>
         <img src={arrowRightIcon} onClick={previousItem} />
       </IconButton>
-    </section>
+    </motion.section>
   );
 }
