f5c12705ad18f376d46ebf1e314a495598ca37a4
Nataniel Santos <nathan.santos159@hotmail.com>
2022-04-28 21:41:46 -0300

    Adiciona animação ao clicar em card na tela inicial

--- src/components/MainPageGrid.js
+++ src/components/MainPageGrid.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import { Backdrop } from '@mui/material';
+import { AnimatePresence, AnimateSharedLayout } from 'framer-motion';
+import React, { useState } from 'react';
 import projects from '../projects';
 import ProjectCard from './ProjectCard';
 
 export default function MainPageGrid() {
+  const [selectedId, setSelectedId] = useState(null);
+
   return (
-    <section className="projects-container-home-wrapper">
-      {projects.slice(0, 6).map((proj, i) => (
-        <ProjectCard {...proj} key={i} />
-      ))}
-    </section>
+    <AnimateSharedLayout type="crossfade">
+      <section className="projects-container-home-wrapper">
+        {projects.slice(0, 6).map((proj, i) => (
+          <ProjectCard
+            {...proj}
+            key={i}
+            framerMotionProps={{ onClick: () => setSelectedId(proj.id), layoutId: proj.id }}
+          />
+        ))}
+      </section>
+      <AnimatePresence>
+        {selectedId && (
+          <Backdrop open={open} onClick={() => setSelectedId(null)} sx={{zIndex: 6}} >
+            <div className="project-card-open">
+              <ProjectCard
+                {...projects[selectedId - 1]}
+                framerMotionProps={{ layoutId: selectedId }}
+              />
+            </div>
+          </Backdrop>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
   );
 }
