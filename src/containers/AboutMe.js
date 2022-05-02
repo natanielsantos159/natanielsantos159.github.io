9626f65044b8bcbf421395f934df17c772f39df1
Nataniel Santos <nathan.santos159@hotmail.com>
2022-05-01 23:46:07 -0300

    Refatora seção AboutMe

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -1,24 +1,33 @@
 import React, { useRef } from 'react';
 import useOnScreen from '../hooks/useOnScreen';
 import profilePicture from '../images/profile-picture.jpg';
+import downloadIcon from '../images/download-icon.png';
+import downloadHover from '../images/download-icon-hover.png';
 
 import '../styles/AboutMe.css';
+import DefaultButton from '../components/DefaultButton';
 
 export default function AboutMe() {
   const ref = useRef();
   useOnScreen(ref, '-250px', 'home');
 
   return (
-    <div className="welcome-container" ref={ref}>
+    <div className="about-me-container" ref={ref}>
+      <h1>Sobre mim</h1>
       <div className="info-wrapper">
         <div>
-          <h1>Oii, tudo bem?</h1>
           <p className="about-me-home">
             Me chamo Nataniel Santos, tenho 20 anos e moro em Santa inês - MA. Atualmente estou
-            estudando Desenvolvimento Web(Full Stack) na Trybe. Sempre fui apaixonado por
-            tecnologia, já tenho um bom conhecimento em front-end e estou dando os meus primeiros
-            passos em back-end!
+            finalizando o curso Desenvolvimento Web Full Stack na Trybe e sou formado em Análise e
+            Desenvolvimento de Sistemas. Sempre fui fascinado por tecnologia e no seu poder de gerar
+            inovação e oportunidade na vida das pessoas.
           </p>
+          <DefaultButton
+            name="Download CV"
+            href="https://drive.google.com/file/d/1oGv6JgZv-YJFtQD5uTiT1YSXYjVe3Yjp/view?usp=sharing"
+            icon={downloadIcon}
+            iconHover={downloadHover}
+          />
         </div>
         <img className="profile-picture" src={profilePicture} alt="Foto de Perfil" />
       </div>
